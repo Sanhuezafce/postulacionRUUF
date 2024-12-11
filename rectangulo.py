a08543b46704b8d2b2f2e7cc3f1e48977b06e595
Sanhuezafce <sanhuezafce@gmail.com>
2024-12-11 11:45:44 -0300

    update de casos bases y correccion en logica. Efectivamente, habia que achicar el rect grande por la otra dim

--- rectangulo.py
+++ rectangulo.py
@@ -4,31 +4,40 @@
 # Retorna cuantos rectangulos de medida a,b caben en un rectangulo de medidas x,y
 # Ej: cuantosCaben(1,2,2,3) retorna 3
 def cuantosCaben(a,b,x,y):
-    # Check area 
-    #ver 2 casos y el max entre ellos
-    #a en el x
-    #b en el x
+    # Casos bordes 
+    
+    # Algun lado mide 0
+    if min(a,b,x,y)<1:
+        return 0
+    # Alguno de los lados del rect pequeno
+    # es mayor que ambos lados del rect grande
+    if (a>x and a>y) or (b>x and b>y):
+        return 0
+
+    #Ambos rectangulos son iguales
+    if (a==x and b==y) or (a==y and b==x):
+        return 1
+        
     ancho1 = int(x/a)
     alto1 = int(y/a)
     ancho2 = int(x/b)
     alto2= int(y/b)
     
-    #chequeo que el rectangulo chico alcance al menos
-    #1 vez en el grande.
+    #Chequeo para ver si cabe algun rectangulo pequeno en el grande
     if (ancho1 < 1) or (alto1 < 1):
         return 0
     else:
-        #Simula teselar desde el ancho y largo
+        #Se tesela el rect pequeno y se achica el grande
         forma1 = ancho1 + cuantosCaben(a,b,x,y-b)
-        forma2 = alto1 + cuantosCaben(a,b,x,y-a)
+        forma2 = alto1 + cuantosCaben(a,b,x-b,y)
         
     if (ancho2 < 1) or (alto2 < 1):
         return 0
     else:    
-        #Simula teselar desde el ancho y largo por el otro
-        #lado del rectangulo
+        #Mismo que para forma1, 2
         forma3 = ancho2 + cuantosCaben(a,b,x,y-a)
-        forma4 = alto2 + cuantosCaben(a,b,x,y-b)
+        forma4 = alto2 + cuantosCaben(a,b,x-a,y)
+        
     return max(forma1,forma2,forma3,forma4)
 #tests
 
